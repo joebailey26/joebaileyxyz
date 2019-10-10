ce81a355de534b3edbd0a1ecb4dc3277076142f8
Joe <joe@joebailey.xyz>
2019-10-10 20:04:11 +0100

    Added CodePen and Twitter to social links

--- index.php
+++ index.php
@@ -261,6 +261,9 @@
 							<a title="GitHub" aria-label="GitHub Social Profile Link" href="https://github.com/joebailey26" rel="noreferrer" target="_blank">
 								<i class="fab fa-github"></i>
 							</a>
+							<a title="CodePen" aria-label="CodePen Social Profile Link" href="https://codepen.io/" rel="noreferrer" target="_blank">
+								<i class="fab fa-codepen"></i>
+							</a>
 						</div>
 					</div>
 				</div>
