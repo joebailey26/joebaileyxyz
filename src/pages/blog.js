7837c486c50e2a36496fbeb1c4141ef0143f99bd
Joe <joe@joebailey.xyz>
2020-04-28 13:19:10 +0100

    Updated layout import

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Share from "../components/share"
 
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 import {picture, tech, button} from "../components/individualItem"
 
